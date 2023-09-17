c1e189d6e5589c5e5ae2052bdf4c1de60eabdb7a
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-09-17 11:15:05 +0530

    custom-rules added

--- Tutorial-3/app/getPayments.js
+++ Tutorial-3/app/getPayments.js
@@ -2,8 +2,8 @@ function getPayments() {
 	return 1;
 }
 
-function getLatestPayments() {
-	return 'latest payments';
-}
+// function getLatestPayments() {
+// 	return 'latest payments';
+// }
 
 getPayments();
--- Tutorial-3/app/rules/findPayments.js
+++ Tutorial-3/app/rules/findPayments.js
@@ -1,12 +1,17 @@
-module.exports = {
-    // meta: {}
-    create(context) {
-        return {
-            CallExpression(node) {
-                if (node.callee.name === getPaymets) {
+// https://astexplorer.net/ --> To understand CallExpression
 
-                }
-            }
-        }
-    }
-}
+module.exports = {
+	// meta: {}
+	create(context) {
+		return {
+			CallExpression(node) {
+				if (node.callee.name === 'getPayments') {
+					context.report({
+						node,
+						message: 'getPayments is deprecated. use getLatestPayments instead',
+					});
+				}
+			}
+		};
+	}
+};
