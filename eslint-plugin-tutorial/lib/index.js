7ef1ea830b647f538bac7630308d686c3829d96e
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-09-20 17:24:39 +0530

    created a shareable plugin and added custom-rules into it

--- eslint-plugin-tutorial/lib/index.js
+++ eslint-plugin-tutorial/lib/index.js
@@ -19,5 +19,13 @@ const requireIndex = require("requireindex");
 module.exports.rules = requireIndex(__dirname + "/rules");
 
 
-
 // Run 'npm link'
+
+/*
+    To create a shareable plugin:
+        --  npm i -g yo
+        --  npm i -g generator-eslint
+        --  yo eslint:plugin
+
+    The Project is created based on eslint-plugin-<plugin_ID>
+*/
